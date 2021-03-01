d9fc2ba327bc6c0cdd24d72f7610fb7bf6fae63f
bashforger <bash.forger@gmail.com>
2021-03-01 19:21:11 +0500

    Added Session COntroller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+class SessionsController < ApplicationController
+
+  def new; end
+
+  def create
+    @user = User.find_by_username(params[:username])
+    if @user
+      session[:user_id] = @user.id
+      redirect_to root_url, notice: 'Signed in successfully!'
+    else
+      flash.now[:alert] = 'Username is invalid'
+      render 'new'
+    end
+  end
+
+  def destroy
+    reset_session
+    redirect_to root_url, notice: 'Signed out successfully!'
+  end
+
+end
