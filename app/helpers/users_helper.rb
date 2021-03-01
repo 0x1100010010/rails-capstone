2eb8e3a4dbf5d46ad5d884780b5e98d6835c5ba1
bashforger <bash.forger@gmail.com>
2021-03-01 20:03:27 +0500

    Fixed session issues

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -8,4 +8,13 @@ module UsersHelper
   def user_params
     params.require(:user).permit(:name, :username)
   end
+
+  def start_session(user)
+    if user
+      session[:user_id] = user.id
+      redirect_to root_url, notice: "User #{user.name} account signed up successfully!"
+    else
+      flash.now[:alert] = 'Username is invalid'
+    end
+  end
 end
