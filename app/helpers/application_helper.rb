168e7da95d66bcf06c24d0598670cbc533bb98a1
bashforger <bash.forger@gmail.com>
2021-03-01 19:54:06 +0500

    Added session helper in application_helper.rb

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+
+  def current_user
+    if session[:user_id]
+      @current_user ||= User.find(session[:user_id])
+    else
+      @current_user = nil
+    end
+  end
+
+  def require_session
+    redirect_to signup_path, alert: 'Sign Up or Sign In to access this feature!' unless current_user
+  end
+
 end
