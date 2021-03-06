2d4102dcb94dc49b36ba4118116f083480fe75ee
bashforger <bash.forger@gmail.com>
2021-03-07 02:17:33 +0500

    Migrated Schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_01_135353) do
+ActiveRecord::Schema.define(version: 2021_03_06_193251) do
+
+  create_table "transactions", force: :cascade do |t|
+    t.string "name"
+    t.string "amount"
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_transactions_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -20,4 +29,5 @@ ActiveRecord::Schema.define(version: 2021_03_01_135353) do
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
+  add_foreign_key "transactions", "users"
 end
