f340ca7241b6ed39a350d73eabe0c6bd9e9fa745
bashforger <bash.forger@gmail.com>
2021-03-09 20:12:49 +0500

    Ran Migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_06_193251) do
+ActiveRecord::Schema.define(version: 2021_03_08_150754) do
+
+  create_table "groups", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.string "icon"
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_groups_on_user_id"
+  end
 
   create_table "transactions", force: :cascade do |t|
     t.string "name"
@@ -29,5 +39,6 @@ ActiveRecord::Schema.define(version: 2021_03_06_193251) do
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
+  add_foreign_key "groups", "users"
   add_foreign_key "transactions", "users"
 end
