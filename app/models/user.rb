6c0d56c896422e3fa1342f88584e6bdee94764db
bashforger <bash.forger@gmail.com>
2021-03-09 20:12:15 +0500

    Updated User Model to reflect Group relation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ApplicationRecord
   validates :username, presence: true, uniqueness: true, length: { maximum: 20 }
 
   has_many :transactions, dependent: :destroy
+  has_many :groups
 end
