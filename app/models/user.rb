73c411af06820304027777edac682f2b44393a79
bashforger <bash.forger@gmail.com>
2021-03-11 19:09:38 +0500

    Added external_transactions & total_amount methods to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,14 @@ class User < ApplicationRecord
 
   has_many :transactions, dependent: :destroy
   has_many :groups
+
+  def external_transactions
+    transactions.select { |t| t.transaction_group_ids.empty? }
+  end
+
+  def total_amount
+    sum=0
+    transactions.each { |t| sum+=t.amount.to_i }
+    sum
+  end
 end
