da4105d7e3bea3d629f99a0d29072a2f778748ab
bashforger <bash.forger@gmail.com>
2021-03-11 19:10:07 +0500

    Added group association in transaction.rb

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,4 +1,8 @@
 class Transaction < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'user_id'
   has_many :transaction_groups
+
+  def group_id
+    transaction_groups.first.group_id unless transaction_group_ids.empty?
+  end
 end
