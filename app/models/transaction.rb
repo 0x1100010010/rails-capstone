37cdd6b36e45bd25b4b7df3ea818fc8c3ececb2b
bashforger <bash.forger@gmail.com>
2021-04-05 20:20:47 +0500

    Added group_id to Transaction Model

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -2,6 +2,10 @@ class Transaction < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'user_id'
   has_many :transaction_groups, dependent: :destroy
 
+  def group_ids
+      transaction_groups.map { |t| t.group_id }
+  end
+
   def group_id
     transaction_groups.first.group_id unless transaction_group_ids.empty?
   end
