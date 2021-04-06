76e98c6dc14a10f5b8593b96ff428038121a8580
bashforger <bash.forger@gmail.com>
2021-04-06 12:27:34 +0500

    Added Transaction methods to model

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,12 @@
 class Group < ApplicationRecord
   belongs_to :owner, class_name: 'User', foreign_key: 'user_id'
   has_many :transaction_groups, dependent: :destroy
+
+  def transaction_ids
+    transaction_groups.map { |t| t.transaction_id }
+  end
+  
+  def transaction_sum
+    transaction_ids.map { |id| Transaction.find(id).amount.to_i }.sum
+  end
 end
