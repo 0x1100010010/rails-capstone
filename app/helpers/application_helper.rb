e609908c839ce181b492466beea9deccec9f22a8
bashforger <bash.forger@gmail.com>
2021-04-11 22:56:11 +0500

    Created method for header logic

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -24,4 +24,21 @@ module ApplicationHelper
       form.submit 'Create', class: 'btn btn-primary py-3 text-light mb-2 shadow-0'
     end
   end
+
+  def render_header
+    html_out = ''
+    if current_user
+      html_out << "<li class='nav-item'><%= link_to 'Groups', groups_path, class: 'nav-link px-3' %></li>"
+      html_out << "<li class='nav-item'><%= link_to 'Transactions', transactions_path, class: 'nav-link px-3' %></li>"
+      html_out << "<li class='nav-item'><%= link_to 'Extenal Transactions', external_transactions_path, class: 'nav-link px-3' %></li>"
+      html_out << "<li class='nav-item'><%= link_to current_user.name, current_user, class: 'nav-link px-3' %></li>"
+      html_out << "<li class='nav-item'><%= link_to 'Sign Out', signout_path, class: 'nav-link px-3' %></li>"
+    else
+      html_out << "<li class='nav-item'>
+                <%= link_to 'Sign In', signin_path, class: 'nav-link px-3'if current_page?(signup_path) %>
+                <%= link_to 'Sign Up', signup_path, class: 'nav-link px-3' if current_page?(signin_path) %>
+              </li>"
+    end
+    render inline: html_out
+  end
 end
