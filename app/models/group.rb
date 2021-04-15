d6431531bf2beb8180af48f06c601d716442dd19
bashforger <bash.forger@gmail.com>
2021-04-15 10:21:02 +0500

    Updated Validations

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,6 +1,12 @@
 class Group < ApplicationRecord
+  include ActiveModel::Validations
+
   belongs_to :owner, class_name: 'User', foreign_key: 'user_id'
   has_many :transaction_groups, dependent: :destroy
+
+  validates :name, length: { maximum: 30 }
+  validates :description, presence: true, length: { maximum: 300 }
+  validates :icon, presence: true
 
   def transaction_ids
     transaction_groups.map(&:transaction_id)
